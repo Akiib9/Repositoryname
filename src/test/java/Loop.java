b02d54947d0ca042342a1280c757e620bb652be2
Akeebshaikh <aquibshaikh700@gmail.com>
2022-09-24 17:05:33 +0530

    Abhijit is a hero

---
+++ src/test/java/Loop.java
@@ -0,0 +1,12 @@
+
+public class Loop {
+
+	public static void main(String[] args) {
+		 for(int i=1;i<=12;i=i+1)
+		 {
+			 System.out.println(i);
+		 }
+         System.out.println("Loop is completed");
+	}
+
+}
---
+++ src/test/java/Nested_Loop.java
@@ -0,0 +1,21 @@
+
+public class Nested_Loop {
+
+	public static void main(String[] args) {
+		for(int r=1;r<=4;r++)
+		{
+			//System.out.print("");
+			for(int c=1;c<=4;c++)
+			{
+				System.out.print("#");
+				for(int d=1;d<=2;d++)
+				{
+					System.out.print("--");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
